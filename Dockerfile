6fdedbd2fbc9edc3473f4a0d3ba392dfef5abc92
Ubuntu <hltesting@jagadeesh-workspace.wgnbaor05qmevhz1jrdvevfj1c.ix.internal.cloudapp.net>
2022-06-28 12:16:46 +0000

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM ghcr.io/petermetz/dci-lint:0.6.1
+
+USER root
+RUN apt update && apt install -y jq
+USER ${APP_USER}
+
+# Copies your code file from your action repository to the filesystem path `/` of the container
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+
+WORKDIR /usr/src/app/
+
+# Executes `docker-entrypoint.sh` when the Docker container starts up
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Request JSON:"
+echo "$1"
+echo "::lint-git-repo-request::Sending request to DCI Lint API..."
+
+RESPONSE=`node /usr/src/app/node_modules/@dci-lint/cmd-api-server/dist/lib/main/typescript/cmd/dci-lint-cli.js lint-git-repo --request="$1"`
+LINTER_EXIT_CODE=$?
+echo "DCI-Lint Process Exit Code: $LINTER_EXIT_CODE"
+echo "DCI-Lint JSON Response:"
+echo "$RESPONSE"
+
+
+LINTER_ERROR_COUNT=`echo $RESPONSE | jq -r '.linterErrors | length'`
+
+echo "::set-output name=lint-git-repo-response::${RESPONSE//[$'\t\r\n ']}"
+echo "::set-output name=lint-git-repo-error-count::$LINTER_ERROR_COUNT"
+echo "::set-output name=lint-git-repo-exit-code::$LINTER_EXIT_CODE"
+
+if [ "$LINTER_EXIT_CODE" -gt "0" ]; then
+  echo "DCI-Lint exited with code $LINTER_EXIT_CODE. Failing the check..."
+  exit 2
+elif [ "$LINTER_ERROR_COUNT" -gt "0" ]; then
+  echo "Found $LINTER_ERROR_COUNT linter errors. Failing the check..."
+  exit 1
+else
+  echo "No linter errors found. Passing the check..."
+  exit 0
+fi
